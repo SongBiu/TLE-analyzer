b34bd205e32490d507eb1b8335679680666c64e5
songl <songlcis@gmail.com>
2019-03-22 09:40:47 +0800

    sync

---
+++ build-release/src/CMakeFiles/analyzer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/analyzer.dir/application.cpp.o"
+  "../analyzer.pdb"
+  "../analyzer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/analyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
