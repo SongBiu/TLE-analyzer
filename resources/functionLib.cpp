30ee521852b8206d438b180ac711ae21a12ce640
lisong <song_pku@foxmail.com>
2019-04-21 00:17:15 +0800

    sync

--- resources/functionLib.cpp
+++ resources/functionLib.cpp
@@ -6,13 +6,10 @@ int h;
 int history[HMAX];
 int size = 0;
 
-void dump(int v) { std::cout << v << std::endl; }
-
 void initHash() { h = 0; }
 
 void addhash(int var) {
     h = h * 31 + var;
-    // std::cout << "tmp hash is " << h << std::endl;
 }
 
 void dumpHash() {
